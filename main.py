833ca5f2d3526bb95c45ae3aa1f7b02bc975c600
mu-suke <chelsea_08ym@yahoo.co.jp>
2020-08-18 01:54:08 +0900

    リクエスト時にjsonを渡す処理に変更

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+# If you run it, execute this command => "uvicorn main:app --reload"
+
 # FastAPI
 from fastapi import FastAPI
+# Pydantic
+from pydantic import BaseModel
+from typing import Optional, List
+
+
+class Item(BaseModel):
+    man_tile: Optional[str] = None
+    pin_tile: Optional[str] = None
+    sou_tile: Optional[str] = None
+    winning_tile: dict
+    melds: Optional[List[dict]] = None
+    dora_indicators: Optional[List[dict]] = None
+    is_tsumo: Optional[bool] = False
+    is_riichi: Optional[bool] = False
+
 # Mahjong API
 from mahjong.hand_calculating.hand import HandCalculator
 from mahjong.tile import TilesConverter
@@ -25,7 +42,7 @@ async def read_item(winning_tile: str, man_tile: str = None, pin_tile: str = Non
     # アガリ牌(ソーズの5)
     win_tile = TilesConverter.string_to_136_array(sou=winning_tile)[0]
 
-    # 鳴き(なし)
+    # 鳴き(チー:CHI, ポン:PON, カン:KAN(True:ミンカン,False:アンカン), カカン:CHANKAN, ヌキドラ:NUKI)
     melds = None
 
     # ドラ(なし)
@@ -38,3 +55,24 @@ async def read_item(winning_tile: str, man_tile: str = None, pin_tile: str = Non
 
     return {"result_main": result.cost['main'], "result_additional": result.cost['additional'], "yaku": result.yaku}
 
+@app.post('/calculation')
+async def test(item:Item):
+    calculator = HandCalculator()
+    # アガリ形(man=マンズ, pin=ピンズ, sou=ソーズ, honors=字牌)
+    tiles = TilesConverter.string_to_136_array(man=item.man_tile, pin=item.pin_tile, sou=item.sou_tile)
+
+    # アガリ牌(ソーズの5)
+    win_tile = TilesConverter.string_to_136_array(man=item.winning_tile['man'], pin=item.winning_tile['pin'], sou=item.winning_tile['sou'], honors=item.winning_tile['honors'])[0]
+
+    # 鳴き(チー:CHI, ポン:PON, カン:KAN(True:ミンカン,False:アンカン), カカン:CHANKAN, ヌキドラ:NUKI)
+    melds = item.melds if not None else None
+
+    # ドラ(なし)
+    dora_indicators = item.dora_indicators if not None else None
+
+    # オプション(なし)
+    config = HandConfig(is_tsumo=item.is_tsumo, is_riichi=item.is_tsumo) if not None else None
+
+    result = calculator.estimate_hand_value(tiles, win_tile, melds, dora_indicators, config)
+
+    return {"result_main": result.cost['main'], "result_additional": result.cost['additional'], "yaku": result.yaku}
